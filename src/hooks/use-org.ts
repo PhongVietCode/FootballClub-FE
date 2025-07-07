2e69c1e51001921ef6c7f86690665608143ebd5c
PhongVietCode <quocphogn1101@gmail.com>
2025-07-08 01:42:54 +0700

    fix yarn run build

--- src/hooks/use-org.ts
+++ src/hooks/use-org.ts
@@ -4,7 +4,7 @@ import type { RootState } from "@/store"
 
 export function useOrgId() {
   const choosenOrg = useAppSelector((state: RootState) => state.user.choosenOrg)
-  const [orgId, setOrgId] = React.useState(
+  const [orgId] = React.useState(
     choosenOrg ? choosenOrg.id : localStorage.getItem("orgId")
   )
 
